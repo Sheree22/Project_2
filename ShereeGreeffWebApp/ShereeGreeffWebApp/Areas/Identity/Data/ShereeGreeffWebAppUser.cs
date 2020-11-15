17d490124b5a7e4012dde335dd199ed2c064dbec
sheree <shereegreeff0@gmail.com>
2020-11-15 20:28:15 +0200

    register_and_login

---
+++ ShereeGreeffWebApp/ShereeGreeffWebApp/Areas/Identity/Data/ShereeGreeffWebAppUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema; //include
+
+namespace ShereeGreeffWebApp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ShereeGreeffWebAppUser class
+    public class ShereeGreeffWebAppUser : IdentityUser
+    {
+        //properties 
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")] //datatype for column
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")] //datatype for column 
+        public string LastName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "int")] //datatype for column 
+        public int Age { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "int")] //datatype for column 
+        public int Number { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(10)")] //datatype for column 
+        public string Gender { get; set; }
+    }
+}
---
+++ ShereeGreeffWebApp/ShereeGreeffWebApp/Migrations/20201115164442_Create.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShereeGreeffWebApp.Migrations
+{
+    public partial class Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "AspNetUsers");
+        }
+    }
+}
